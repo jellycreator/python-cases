d1dcc744e1cdf2b970a8cb58f75f42ed9b0c88b9
jellycreator <577430840@qq.com>
2021-11-29 10:47:43 +0800

    qwq

---
+++ 8.6-随机生成验证码.py
@@ -0,0 +1,10 @@
+import random
+
+def verificationv_code(long=6):
+    li_random = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    li_id = random.choices(li_random, k=long)
+    result = ''
+    for i in li_id:
+        result = result + i
+    print('验证码是 %s' % result)
+verificationv_code()
